87491417154b5eab33150f62150e54c9b5e14ae4
SpottedEleCN <87370213+xiaochenhao576@users.noreply.github.com>
2023-09-10 12:01:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -22,7 +22,8 @@ const BLOG = {
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://ofpepchinese.eu.org', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'OFPEP, FNF', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'pandaman@ofpepchinese.eu.org', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_EMAIL2: process.env.NEXT_PUBLIC_CONTACT_EMAIL2 || 'pandaman_reserve@ofpepchinese.eu.org',
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
