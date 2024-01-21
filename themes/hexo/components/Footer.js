a591eeb7801ea260d35f709c5348fe33f324bb08
byguage <110472388+byguage@users.noreply.github.com>
2024-01-21 22:55:50 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = ({ title }) => {
     >
       {/* <DarkModeButton/> */}
 
-      <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.<a href="https://icp.gov.moe/?keyword=20240085" target="_blank">萌ICP备20240085号</a><br/>
+      <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a><i className='mx-1 animate-pulse fas fa-heart'/><a href="https://icp.gov.moe/?keyword=20240085" target="_blank">萌ICP备20240085号.</a><br/>
 
       {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
 
